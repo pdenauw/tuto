5568a963592b394d43c898ff393d9c7c575bdb54
Pierre Denauw <pierre.denauw@telecomnancy.eu>
2021-01-07 13:52:43 +0100

    Ajout fichiers non traqué

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour
+{
+    private Transform spawn;
+    private Animator fadeSystem;
+
+    private void Awake()
+    {
+        spawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(replace(collision));
+        }
+    }
+
+    private IEnumerator replace(Collider2D collision)
+    {
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        collision.transform.position = spawn.position;
+
+    }
+}
---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    private bool isInRange;
+    private PlayerMove playerMove;
+    public BoxCollider2D topCollider;
+
+    void Awake()
+    {
+        playerMove = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRange && playerMove.isClimbing && Input.GetKeyDown(KeyCode.E))
+        {
+            playerMove.isClimbing = false;
+            topCollider.isTrigger = false;
+            return;
+        }
+        if(isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            playerMove.isClimbing = true;
+            topCollider.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+            playerMove.isClimbing = false;
+            topCollider.isTrigger = false;
+        }
+    }
+}
